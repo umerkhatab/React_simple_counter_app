8cc1753e06013bfb40665662edd92fae8fca42ac
Umer Khatab <abbasiumerkhatab@gmail.com>
2020-08-25 15:25:34 +0500

    simple counter app in react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import Message from './message.js';
 import './App.css';
+export default function App() {
 
-function App() {
+  let [count, setCount] = useState(0)
+  let [isMorning,setMorning] = useState(true)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    
+    <div className={`box ${isMorning ? 'daylight' : ''}`}>
+      <h1> Good {isMorning ? 'Morning' : 'Night'}</h1>
+      <Message counter={count}/>
+      <br />
+      <button onClick={
+        ()=> setCount(count + 1)
+        }>Update counter</button>
+
+        <button onClick={()=>setMorning(!isMorning)
+        }>Update day
+
+        </button>
     </div>
   );
 }
-
-export default App;
---
+++ src/message.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Message (props) {
+
+    return(
+        <h2>Value of counter is : {props.counter} </h2>
+
+    )
+}
